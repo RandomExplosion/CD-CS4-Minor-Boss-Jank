e7eff46fef24844bc76e7cdfe1deb8762212fc9d
RandomExplosion <benjaminjmcgaw@gmail.com>
2020-02-27 07:01:19 +1100

    Switch to .Net Core

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿using Habanero.Util;
-using System;
+﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace CSharp_MinorBoss_ConsoleApp
 {
@@ -11,12 +11,12 @@ namespace CSharp_MinorBoss_ConsoleApp
         /// </summary>
         static void WriteTest()
         {
-            
+
         }
-        
+
         static void ReadTest()
         {
-            
+
         }
 
         static void Main(string[] args)
@@ -25,14 +25,14 @@ namespace CSharp_MinorBoss_ConsoleApp
             {
                 #region Scope:RootMenu
                 int input = 0;  //Reused throughout entire program
-                string[] options = { "View Marvel DB", "Add Superhero", "Edit Superhero", "Remove Superhero" };  //Reused throughout the program
+                string[] options = { "View Marvel DB", "Add Superhero", "Edit Superhero", "Remove Superhero", "Exit System" };  //Reused throughout the program
 
                 input = QueryUserAction("Welcome To MarvelDB Superhero Database Management!\nWhat would you like to do?", options);
                 switch (input)
                 {
                     case 1:
-                        Console.WriteLine("Not Yet Implimented");
-                        CSVParser.GetCSV("Hello World");
+                        //Console.WriteLine("Not Yet Implimented");
+                        CSVParser.GetCSV("Database.csv");
                         WaitForEnterPress();
                         break;
 
@@ -50,6 +50,9 @@ namespace CSharp_MinorBoss_ConsoleApp
                         Console.WriteLine("Not Yet Implimented");
                         WaitForEnterPress();
                         break;
+
+                    case 5:
+                        return;
 
                         //No need for default state as validation is handled in QueryUserAction()
 
@@ -71,7 +74,7 @@ namespace CSharp_MinorBoss_ConsoleApp
                 for (int i = 0; i < optionsList.Length; i++)                    //Print Options
                 {
                     Console.WriteLine($"{i + 1}. {optionsList[i]}");
-                }  
+                }
 
                 try                                                             //Gather Input and Verify that it is in range
                 {
@@ -90,11 +93,11 @@ namespace CSharp_MinorBoss_ConsoleApp
                 {
                     continue;                                               //Allow the user to try again
                 }
-                
+
             }
 
             return inputInt;
-            
+
         }
 
         static void WaitForEnterPress()
@@ -108,23 +111,49 @@ namespace CSharp_MinorBoss_ConsoleApp
                 if (keyPress.Key == ConsoleKey.Enter)
                 {
                     hasPressedEnter = true;
-                } 
+                }
             }
         }
     }
 
-    //I am using a .Net framework app not .Net Core
-    //Therefore the example parser does not work - So i wrote my own ;)
-    class CSVParser
+    public class SuperHero
     {
-        public static void GetCSV(string fileName)
+        public string heroName = "";
+        public string realName = "";
+        public double weight = 0d;
+        public double height = 0d;
+
+        SuperHero(string _heroName, string _realName, double _weight, double _height)
         {
-            string fullpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "MarvelDB", fileName);
-
-            if (File.Exists(fullpath))
-            {
-
-            }
+            heroName = _heroName;
+            realName = _realName;
+            weight = _weight;
+            height = _height;
         }
+
+        void ModifyData(string _heroName, string _realName, double _weight, double _height)
+        {
+            heroName = _heroName;
+            realName = _realName;
+            weight = _weight;
+            height = _height;
+        }
+    }
+    public class HeroTable
+    {
+        protected SortedDictionary<string, SuperHero> heroesByName;
+
+        public static HeroTable _tableInstance;
+
+        static void OnDbUpdate()
+        {
+
+        }
+
+        static string GetTableAsString()
+        {
+            return "Table printing not yet implimented";
+        }
+
     }
 }
